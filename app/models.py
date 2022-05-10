6b653a82cb473a1a1aa4f76003a95ffe535fe238
Ann Wanjeri <wannjeri2@gmail.com>
2022-05-10 22:31:24 +0300

    password hashing for User class

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 # creating model to define user data
+from msilib.schema import Property
 from . import db 
+from werkzeug.security import generate_password_hash,check_password_hash
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -7,6 +9,21 @@ class User(db.Model):
     username = db.Column(db.String(255),index=True,unique=True)
     email = db.Column(db.String(200), index = True, unique=True)
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    pass_secure = db.Column(db.String(255))
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot red the password attribute')
+
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure=generate_password_hash(password)
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+
+    
 
 
     def __repr__(self):
@@ -17,6 +34,7 @@ class Role(db.Model):
 
     id = db.Column(db.Integer,primary_key = True)
     name = db.Column(db.String(255))
+    users = db.relationship('User',backref ='role',lazy='dynamic')
 
     def __repr__(self):
         return f'User{self.name}'
